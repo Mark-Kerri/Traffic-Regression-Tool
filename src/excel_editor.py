670dbcd6afec3937c237dc5449a935a65bfae1e8
Mark-Kerri <mark.kerri@steergroup.com>
2024-07-02 16:07:42 +0100

    Update excel editor and template

--- src/excel_editor.py
+++ src/excel_editor.py
@@ -1,17 +1,66 @@
-from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
+import openpyxl
 
 
-# Function to create the input template
-def create_input_template(independent_vars, output_path):
-    # Load the base template
-    wb = load_workbook("base_template.xlsx")
+def create_input_template(client_name, project_name, y_variables, x_variables):
+    # Define the path to the template file
+    template_path = "data/utils/excel_template_v0.01.xlsx"
+
+    # Load the template workbook
+    wb = openpyxl.load_workbook(template_path)
     ws = wb.active
 
-    # Set headers for independent variables
-    for i, var in enumerate(independent_vars, start=3):
-        col_letter = get_column_letter(i)
-        ws[f"{col_letter}1"] = var
+    # Define the values to be updated
+    file_name = f"{project_name} Regression Inputs"
+    heading = "Regression Inputs"
 
-    # Save the modified template
+    # Update the specified cells
+    ws["B2"] = client_name
+    ws["B3"] = project_name
+    ws["B4"] = file_name
+    ws["B6"] = heading
+
+    # Define y variable names
+    y_row = 10
+
+    ws[f"D{y_row}"] = "Dependent Variables"
+    ws[f"E{y_row}"] = "abs/pct"
+
+    for key, item in y_variables.items():
+        y_row += 1
+
+        ws[f"D{y_row}"] = key
+        ws[f"E{y_row}"] = item
+
+    # Define x variable names
+    x_row = y_row + 2
+
+    ws[f"D{x_row}"] = "Independent Variables"
+    ws[f"E{x_row}"] = "abs/pct"
+
+    for key, item in x_variables.items():
+        x_row += 1
+
+        ws[f"D{x_row}"] = key
+        ws[f"E{x_row}"] = item
+
+    # Define the output path
+    output_path = f"data/reg_input/{file_name}.xlsx"
+
+    # Save the modified workbook
     wb.save(output_path)
+
+
+# Example usage for development
+if __name__ == "__main__":
+    y_variables = {"Traffic (AADT)": "abs"}
+
+    x_variables = {
+        "GDP": "abs",
+        "Unemployment": "pct",
+        "Ramp Up": "abs",
+        "Signage": "abs",
+        "Cong_IC2": "abs",
+        "Cong_A20": "abs",
+    }
+
+    create_input_template("AEDL", "A32 LV Traffic Forecast", y_variables, x_variables)
