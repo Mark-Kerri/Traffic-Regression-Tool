dd8ef0c55f9f362c1f1c024e096bae49a52e65fd
Mark-Kerri <mark.kerri@steergroup.com>
2024-07-15 14:56:21 +0100

    Create new test data file

--- src/process_inputs.py
+++ src/process_inputs.py
@@ -9,35 +9,37 @@ import plotly.graph_objects as go
 # Output dataframe will name each variable before "Independent Variables" cell as X:{variable name} and every variable after "Independent Variables" cell as Y:{variable name}
 # Building the index of the df assuming column G is empty (on all rows above data) and column G is the first time series column
 
-def spreadsheet_to_df(file_name= 'A32 LV Traffic Forecast Regression Inputs Filled.xlsx'):
+
+def spreadsheet_to_df(file_name):
     import pandas as pd
-    file_path = os.path.join('data','reg_input')
-    file_name = 'A32 LV Traffic Forecast Regression Inputs Filled.xlsx'
-    full_path = os.path.join(file_path,file_name)
+
+    # delete below line to test pathing
+    file_path = os.path.join("data", "reg_input")
+    file_name = "Development Test Data Regression Inputs.xlsx"
+    full_path = os.path.join(file_path, file_name)
     workbook = openpyxl.load_workbook(full_path, data_only=True)
     sheet = workbook.active
 
-
     # Read column names and row names so that the dataframe is filled
     df_cols = []
-    dependent_flag =1
-    col = 4 # start from Column D for variable names
-    for row in range(1,sheet.max_row+1):
+    dependent_flag = 1
+    col = 4  # start from Column D for variable names
+    for row in range(1, sheet.max_row + 1):
         if sheet.cell(row=row, column=col).value != None:
             # if sheet.cell(row=row, column=col).value != 'Dependent Variables' and  sheet.cell(row=row, column=col).value != 'Independent Variables' :
-            if sheet.cell(row=row, column=col).value == 'Dependent Variables':
+            if sheet.cell(row=row, column=col).value == "Dependent Variables":
                 dependent_flag = 1
                 continue
-            if sheet.cell(row=row, column=col).value == 'Independent Variables':
+            if sheet.cell(row=row, column=col).value == "Independent Variables":
                 dependent_flag = 0
                 continue
-            if dependent_flag ==1:
-                df_cols.append('y:' + sheet.cell(row=row, column=col).value)
-            elif dependent_flag ==0:
-                df_cols.append('x:' + sheet.cell(row=row, column=col).value)
+            if dependent_flag == 1:
+                df_cols.append("y:" + sheet.cell(row=row, column=col).value)
+            elif dependent_flag == 0:
+                df_cols.append("x:" + sheet.cell(row=row, column=col).value)
 
     # Build dataframe by iterating over the columns for each variable
-    df = pd.DataFrame(columns = df_cols)
+    df = pd.DataFrame(columns=df_cols)
     for c in df_cols:
         temp_list = []
         # print(c)
@@ -54,22 +56,21 @@ def spreadsheet_to_df(file_name= 'A32 LV Traffic Forecast Regression Inputs Fill
                 break
         df[c] = temp_list
 
-
     # Build the index of the df assuming column G is empty and column G is the first time series column
-    col = 7 # Start at Col G
+    col = 7  # Start at Col G
     for row in range(1, sheet.max_row + 1):
         if sheet.cell(row=row, column=col).value != None:
             start_row = row
             print(start_row)
             break
-    idx_row = start_row + 2 # 2 rows below first non-empty cell
+    idx_row = start_row + 2  # 2 rows below first non-empty cell
 
     temp_list = []
     cell_names = sheet[idx_row]
-    for cellObj in cell_names[(col-1):]:
+    for cellObj in cell_names[(col - 1) :]:
         try:
             val = str(cellObj.value)
-            if val != 'None':
+            if val != "None":
                 temp_list.append(val)
         except ValueError:
             break
@@ -77,39 +78,3 @@ def spreadsheet_to_df(file_name= 'A32 LV Traffic Forecast Regression Inputs Fill
     df.index = df_idx
 
     return df
-
-    # fig = go.Figure()
-    #
-    # fig.add_trace(go.Scatter(x=df["x:Traffic (AADT)"], y=df["y:Cong_IC2"], mode='markers', name="x:Traffic (AADT) vs y:Cong_IC2"))
-    # fig.add_trace(go.Scatter(x=df["x:Traffic (AADT) v2"], y=df["y:Cong_IC2"], mode='markers', name="x:Traffic (AADT) v2 vs y:Cong_IC2"))
-    # fig.add_trace(go.Scatter(x=df["x:Traffic (AADT)"], y=df["y:Cong_A20"], mode='markers', name="x:Traffic (AADT) vs y:Cong_A20"))
-    # fig.add_trace(go.Scatter(x=df["x:Traffic (AADT) v2"], y=df["y:Cong_A20"], mode='markers', name="x:Traffic (AADT) v2 vs y:Cong_A20"))
-    #
-    # # Set title and labels
-    # fig.update_layout(
-    #     title="Traffic (AADT) vs Cong_IC2 and Cong_A20",
-    #     xaxis_title="Traffic (AADT)",
-    #     yaxis_title="Congestion"
-    # )
-    #
-    # # Show plot
-    # fig.show()
-    #
-    #
-    # # Create scatter plots
-    # fig = px.scatter(df, x="x:Traffic (AADT)", y="y:Cong_IC2", title="x:Traffic (AADT) vs y:Cong_IC2")
-    # fig.add_scatter(x=df["x:Traffic (AADT) v2"], y=df["y:Cong_IC2"], mode='markers', name="x:Traffic (AADT) v2 vs y:Cong_IC2")
-    # fig.add_scatter(x=df["x:Traffic (AADT)"], y=df["y:Cong_A20"], mode='markers', name="x:Traffic (AADT) vs y:Cong_A20")
-    # fig.add_scatter(x=df["x:Traffic (AADT) v2"], y=df["y:Cong_A20"], mode='markers', name="x:Traffic (AADT) v2 vs y:Cong_A20")
-    #
-    # # Show plot
-    # fig.show()
-    #
-    import pandas as pd
-    import matplotlib as plt
-    import numpy as np
-    import cufflinks as cf
-    # import plotly.express as px
-    #
-    # fig = px.line(df)
-    # fig.show()
